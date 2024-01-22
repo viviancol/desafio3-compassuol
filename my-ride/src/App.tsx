885c91e4026321a352a693f25dadf71c281f48c7
Vívian Oliveira <viviancol2@yahoo.com.br>
2024-01-22 20:46:02 -0300

    feat: inclusion of the navbar component in app.tsx

--- my-ride/src/App.tsx
+++ my-ride/src/App.tsx
@@ -1,12 +1,11 @@
 import './App.css'
+import Navbar from './components/navbar/Navbar'
 
 function App() {
   return (
-    <>
-      <div>
-       My Ride
-      </div>
-    </>
+  <>
+  <Navbar />
+  </>
   )
 }
 
