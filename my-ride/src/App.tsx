ade7bd01d407c33e4988f92fa2199b5a08b43690
Vívian Oliveira <viviancol2@yahoo.com.br>
2024-01-22 23:29:07 -0300

    update: separation of header - section - footer

--- my-ride/src/App.tsx
+++ my-ride/src/App.tsx
@@ -1,12 +1,19 @@
 import './App.css'
+import Footer from './components/footer/Footer';
 import Navbar from './components/navbar/Navbar'
 
 function App() {
   return (
-  <>
-  <Navbar />
-  </>
-  )
+    <>
+      <header>
+        <Navbar />
+      </header>
+      <section></section>
+      <footer>
+        <Footer />
+      </footer>
+    </>
+  );
 }
 
 export default App
