b08ffbb1d24e8c9b4c68a3d1e3667e1ef099cec0
Vívian Oliveira <viviancol2@yahoo.com.br>
2024-01-23 01:33:26 -0300

    feat: new columns in footer.tsx (company - support - more)

--- my-ride/src/components/footer/Footer.tsx
+++ my-ride/src/components/footer/Footer.tsx
@@ -4,46 +4,68 @@ import React from 'react'
 const Footer: React.FC = () => {
   return (
     <Container 
-    component="footer" 
-    sx={{  
-        position:'fixed',
-        mx:'full', 
-        marginTop:'4rem', 
-        bgcolor:'#242424', 
-        color:'#fff', 
-        padding:'40px 80px',
-        width:'100%' 
+        component="footer" 
+        sx={{  
+            position:'fixed',
+            marginTop:'4rem', 
+            bgcolor:'#242424', 
+            color:'#fff', 
+            padding:'40px 80px',
+            width:'100%'                
         }} >
-        <Box display="flex" justifyContent="start" flexWrap="wrap" >
-            <Box>
+        <Box sx={{display:'flex', justifyContent:'start'}}>
+            <Box marginRight='80px'>
                 <img 
                 src="../public/logo-white.png" 
                 alt="Logo do site no rodapé" />
                 <Typography 
                 sx={{my:'16px'}} 
                 variant='body2'>MyRide Inc., 2nd Floor, New York, NY 10016</Typography>
-                
-                <Box >
-                <img 
-                src="../public/Facebook.png" 
-                alt="icone facebook" />
-                <img 
-                src="../public/Instagram.png" 
-                alt="icone instagram" 
-                style={{marginLeft:'20px', marginRight:'20px'}} />
-                <img 
-                src="../public/Twitter.png" 
-                alt="icone twitter" />
-                </Box>
-
                 <Box>
-
+                    <img 
+                    src="../public/Facebook.png" 
+                    alt="icone facebook" />
+                    <img 
+                    src="../public/Instagram.png" 
+                    alt="icone instagram" 
+                    style={{marginLeft:'20px', marginRight:'20px'}} />
+                    <img 
+                    src="../public/Twitter.png" 
+                    alt="icone twitter" />
                 </Box>
+            </Box>
 
+            <Box gap='20px'>
+                <Typography 
+                marginBottom='20px'
+                variant='h6'>Company</Typography>
+                <p>About Us</p>
+                <p>News</p>
+                <p>Careers</p>
+                <p>How we work</p>
+            </Box>
+
+            <Box marginRight='40px' marginLeft='40px'>
+                <Typography 
+                marginBottom='20px'
+                variant='h6'>Support</Typography>
+                <p>FAQ</p>
+                <p>US Office</p>
+                <p>Asia Office</p>
+                <p>Help Center</p>
+            </Box>
+
+            <Box>
+                <Typography 
+                marginBottom='20px'
+                variant='h6'>More</Typography>
+                <p>Become a partner</p>
+                <p>Partner Support</p>
+                <p>Mobile app links</p>
             </Box>
         </Box>
     </Container>
-  )
-}
+  );
+};
 
 export default Footer
