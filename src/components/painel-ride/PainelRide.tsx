cb90e04a19799a21b7ce80a956502dc963875a30
Vívian Oliveira <viviancol2@yahoo.com.br>
2024-01-23 18:52:00 -0300

    feat: cards PainelRide.tsx

--- src/components/painel-ride/PainelRide.tsx
+++ src/components/painel-ride/PainelRide.tsx
@@ -1,5 +1,6 @@
-import { Box, Stack, Typography } from '@mui/material'
+import { Box, Stack, Typography, Button, Card } from '@mui/material'
 import React from 'react'
+import RideCard from './RideCard'
 
 const PainelRide: React.FC = () => {
   return (
@@ -16,6 +17,56 @@ const PainelRide: React.FC = () => {
                     fontSize:'40px',
                 }} >Best in class rides</Typography>
             </Box>
+            <Card>
+                <Box>
+                    <img src=".//public/card-image1.png" alt="Imagem Card 1" />
+                    <RideCard 
+                    title={'Basic'} 
+                    subtitle={'The best balance of price and comfort. You will not go wrong with our basic rides.'}  />
+                    <Button 
+                    variant="text" 
+                    style={{color:'#FBA403'}} 
+                    href="https://www.google.com/search?q=basic+car"
+                    target="_blank"
+                    > Learn More</Button>
+                </Box>
+                <Box>
+                    <img src=".//public/card-image2.png" alt="Imagem Card 2" />
+                    <RideCard 
+                    title={'Confort'} 
+                    subtitle={'If comfort is your priority, this is the ride for you. It’s spacious and packed with features.'}  />
+                    <Button 
+                    variant="text" 
+                    style={{color:'#FBA403'}} 
+                    href="https://www.google.com/search?q=confort+car"
+                    target="_blank"
+                    > Learn More</Button>
+                </Box>
+                <Box>
+                    <img src=".//public/card-image2.png" alt="Imagem Card 2" />
+                    <RideCard 
+                    title={'Business'} 
+                    subtitle={'Do you want to travel around the city in style? Make sure to select or business class rides.'}  />
+                    <Button 
+                    variant="text" 
+                    style={{color:'#FBA403'}} 
+                    href="https://www.google.com/search?q=business+car"
+                    target="_blank"
+                    > Learn More</Button>
+                </Box>
+                <Box>
+                    <img src=".//public/card-image2.png" alt="Imagem Card 2" />
+                    <RideCard 
+                    title={'Deluxe'} 
+                    subtitle={'The best ride for luxury and comfort. No compromises here. You’ll surely get what you pay for.'}  />
+                    <Button 
+                    variant="text" 
+                    style={{color:'#FBA403'}} 
+                    href="https://www.google.com/search?q=carros+de+luxo"
+                    target="_blank"
+                    > Learn More</Button>
+                </Box>
+            </Card>
         </Box>
     </Stack>
   )
