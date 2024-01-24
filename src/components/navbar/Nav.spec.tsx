70c56cc32deb682cb9ceca11e92015b2c88e45eb
Vívian Oliveira <viviancol2@yahoo.com.br>
2024-01-24 20:27:05 -0300

    feat: test of NavBar component

--- src/components/navbar/Nav.spec.tsx
+++ src/components/navbar/Nav.spec.tsx
@@ -1,12 +1,21 @@
-import { render, screen } from '@testing-library/react'
-import '@testing-library/jest-dom/extend-expect'
+import { render, screen, fireEvent } from '@testing-library/react'
 import Navbar from './Navbar'
+import React from 'react'
 
 describe ('Navbar component', () => {
     it('renders navbar components correctly', () => {
         render (<Navbar/>);
 
-
         expect(screen.getByAltText('Logo do Site')).toBeInTheDocument()
-    })
+        expect(screen.getByLabelText('Menu principal')).toBeInTheDocument()
+        expect(screen.getByText('Getting a Taxi')).toBeInTheDocument()
+    });
+    
+    it('changes active tab on click', () => {
+        render (<Navbar/>);
+
+        fireEvent.click(screen.getByText('Getting a Taxi'));
+
+        expect(screen.getByText('Getting a Taxi')).toHaveStyle({color:'#FBA403'});
+    });
 })
